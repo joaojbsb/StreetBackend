2283b7f34066dba125b79fb17f516264fb3a767d
João Oliveira Lima Júnior <47161770+joaojbsb@users.noreply.github.com>
2021-03-02 20:36:26 -0300

    Update databaseBecap.js

--- config/databaseBecap.js
+++ config/databaseBecap.js
@@ -1,6 +1,1 @@
-const mongoose = require('mongoose');
 
-const url = "mongodb+srv://usuario:servidor10@cluster0.heiru.mongodb.net/StreeFighter?retryWrites=true&w=majority";
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
-
-module.exports = mongoose;
